033ad00ddc1d4d68ab9471670c8edad72718dc84
Chris Wright <cmwright33@gmail.com>
2020-05-29 16:29:49 -0400

    update module file structure

---
+++ model/comic.go
@@ -0,0 +1,22 @@
+
+type ComicResponse struct {
+	Month      string `json:"month"`
+	Num        int    `json:"num"`
+	Link       string `json:"link"`
+	Year       string `json:"year"`
+	News       string `json:"news"`
+	SafeTitle  string `json:"safe_title"`
+	Transcript string `json:"transcript"`
+	Alt        string `json:"alt"`
+	Img        string `json:"img"`
+	Title      string `json:"title"`
+	Day        string `json:"day"`
+}
+
+type Comic struct {
+	Title       string `json:"title"`
+	Number      int    `json:"number"`
+	Date        string `json:"date"`
+	Description string `json:"description"`
+	Image       string `json:"image"`
+}
